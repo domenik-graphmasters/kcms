44dfc1909639f80c84bc128f68c9912e2a34a2aa
domenikweber <domenik.weber@graphmasters.net>
2024-03-16 22:36:20 +0100

    💄 Improving the statistics typography

--- modules/mod_internet_json/scripts/components/statistics.php
+++ modules/mod_internet_json/scripts/components/statistics.php
@@ -25,11 +25,28 @@ function renderStatisticsWithExplainer(
         echo "<p class='text-left'>$paragraph</p>";
     }
 
-    echo "<div class='col-xs-12 col-md-4 mt-4'>";
+    echo "<div class='col-xs-12 col-md-3 mt-4'>";
+    renderStat($first);
+    echo '</div>';
+
+    echo "<div class='col-xs-12 col-md-3 mt-4'>";
+    renderStat($second);
+    echo '</div>';
+
+    echo "<div class='col-xs-12 col-md-3 mt-4'>";
+    renderStat($third);
     echo '</div>';
 
 
     echo '</div>';
+}
+
+function renderStat(Stat $stat): void
+{
+    $value = $stat->value;
+    echo "<p class='display-2'>$value</p>";
+    $hint = $stat->hint;
+    echo "<p class=''>$hint</p>";
 }
 
 function renderStatisticsStrip(
@@ -39,7 +56,38 @@ function renderStatisticsStrip(
     Stat $fourth
 ): void
 {
-    echo '<div class="row">';
-    echo "<div class='alert alert-warning' role='alert'>renderStatisticsStrip is not implemented</div>";
+    echo '<div class="container-liquid bg-info">';
+    echo '<div class="row px-4 py-5 my-5 ">';
+
+    echo "<div class='col-xs-12 col-md-3 mt-4'>";
+    $h = $first->value;
+    echo "<p class='h1'>$h</p>";
+    $j = $first->hint;
+    echo "<p class=''>$j</p>";
+    echo '</div>';
+
+    echo "<div class='col-xs-12 col-md-3 mt-4'>";
+    $h = $second->value;
+    echo "<p class='h1'>$h</p>";
+    $j = $second->hint;
+    echo "<p class=''>$j</p>";
+    echo '</div>';
+
+    echo "<div class='col-xs-12 col-md-3 mt-4'>";
+    $h = $third->value;
+    echo "<p class='h1'>$h</p>";
+    $j = $third->hint;
+    echo "<p class=''>$j</p>";
+    echo '</div>';
+
+    echo "<div class='col-xs-12 col-md-3 mt-4'>";
+    $h = $fourth->value;
+    echo "<p class='h1'>$h</p>";
+    $j = $fourth->hint;
+    echo "<p class=''>$j</p>";
+    echo '</div>';
+
+
+    echo '</div>';
     echo '</div>';
 }
