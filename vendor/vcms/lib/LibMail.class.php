b0a392412469354a6e05d3bebbe4545d6dc3f1eb
domenikweber <domenik.weber@graphmasters.net>
2024-10-31 19:57:12 +0100

    🐛 Setting a lower debug level for smtp

--- vendor/vcms/lib/LibMail.class.php
+++ vendor/vcms/lib/LibMail.class.php
@@ -34,7 +34,7 @@ class LibMail{
 		$mail->Port = $smtpPort;
 		$mail->Username = $libGenericStorage->loadValue('base_core', 'smtp_username');
 		$mail->Password = $libGenericStorage->loadValue('base_core', 'smtp_password');
-		$mail->SMTPDebug = 2;
+		$mail->SMTPDebug = \PHPMailer\PHPMailer\SMTP::DEBUG_CONNECTION;
 
 		if($smtpPort == 465) {
 			$mail->SMTPSecure = \PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS;
